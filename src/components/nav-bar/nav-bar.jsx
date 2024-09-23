6f3517b59e938384f8af0b89cd3c71583613fe93
Andrew Marfo <andrewmarfo23@gmail.com>
2024-09-23 22:10:48 +0000

    first commit

---
+++ src/components/nav-bar/nav-bar.jsx
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+class NavBar extends Component{
+
+    render() {
+        return (
+            <div>
+                <h1>
+                    Nav bar
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default NavBar
+
