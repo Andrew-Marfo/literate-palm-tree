5a7d873c684ab9f364f44b11780af8e3639eeb73
Andrew Marfo <andrewmarfo23@gmail.com>
2024-09-23 23:43:00 +0000

    background image

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 
 import NavBar from "./components/nav-bar/nav-bar";
 import './App.css';
+import './components/nav-bar/nav-bar.css'
 
 
 class App extends Component{
