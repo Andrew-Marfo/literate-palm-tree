2a8611064705d4a2556b73cad486ccf418b76187
Andrew Marfo <andrewmarfo23@gmail.com>
2024-09-23 22:31:18 +0000

    nav bar

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -1,13 +1,24 @@
 import { Component } from "react";
 
-class NavBar extends Component{
+class NavBar extends Component {
 
     render() {
         return (
-            <div>
-                <h1>
-                    Nav bar
-                </h1>
+            <div className="nav-bar">
+                <div className="icon">
+                    <h2 className="logo">
+                        PraRoz
+                    </h2>
+                </div>
+                <div className="nav-menu">
+                    <ul className="menu-items">
+                        <li>HOME</li>
+                        <li>ABOUT</li>
+                        <li>SERVICES</li>
+                        <li>DESIGN</li>
+                        <li>CONTACT</li>
+                    </ul>
+                </div>
             </div>
         )
     }
